c3f198014d5fbe904b5d4803eb8f343e77757f24
Yejin-02 <yangyj21@gm.gist.ac.kr>
2025-02-15 09:43:13 +0900

    feat: image control bottom sheet

--- lib/presentation/pages/profile_edit_page.dart
+++ lib/presentation/pages/profile_edit_page.dart
@@ -7,6 +7,7 @@ import 'package:bbun/presentation/widgets/bbun_inputfield.dart';
 import 'package:bbun/presentation/widgets/bbun_pressable.dart';
 import 'package:flutter/material.dart';
 import 'package:bbun/routes/app_router.dart';
+import 'package:flutter_svg/svg.dart';
 
 @RoutePage()
 class ProfileEditPage extends StatefulWidget {
@@ -145,7 +146,117 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
                   SizedBox(
                     width: 321,
-                    child: Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        // 사진 변경 클릭 시 생기는 바텀 시트
+                        showModalBottomSheet(
+                            context: context,
+                            backgroundColor: Colors.transparent,
+                            builder: (BuildContext context) {
+                              return Container(
+                                height: 180,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(16),
+                                    topRight: Radius.circular(16),
+                                  ),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 28, vertical: 5),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      width: 68,
+                                      height: 5,
+                                      decoration: ShapeDecoration(
+                                        color: Color(0xFFE3E3E3),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 25),
+                                    // 라이브러리에서 선택
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          // TODO: 라이브러리에서 선택 로직 구현
+                                          print("라이브러리에서 선택");
+                                          Navigator.pop(context);
+                                        },
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            SvgPicture.asset(
+                                              'assets/icons/gallery.svg',
+                                              width: 18,
+                                            ),
+                                            const SizedBox(width: 15),
+                                            Text(
+                                              '라이브러리에서 선택',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 18,
+                                                fontFamily: 'Pretendard',
+                                                fontWeight: FontWeight.w500,
+                                                height: 1,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 30),
+
+                                    // 현재 사진 삭제
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          // TODO: 현재 사진 삭제 로직 구현
+                                          print("현재 사진 삭제");
+                                          Navigator.pop(context);
+                                        },
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            SvgPicture.asset(
+                                              'assets/icons/delete.svg',
+                                              width: 18,
+                                            ),
+                                            const SizedBox(width: 15),
+                                            Text(
+                                              '현재 사진 삭제',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Color(0xFFF10000),
+                                                fontSize: 18,
+                                                fontFamily: 'Pretendard',
+                                                fontWeight: FontWeight.w500,
+                                                height: 1,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                            });
+                      },
                       child: Text(
                         '사진 변경',
                         textAlign: TextAlign.center,
