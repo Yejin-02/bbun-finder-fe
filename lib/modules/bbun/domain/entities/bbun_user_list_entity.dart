eca0775dfe7b7678fdbb74def3a11bfc17324b4f
andongyeon <a56634368@gmail.com>
2025-02-17 21:50:30 +0900

    feat: add bbun user list entity

---
+++ lib/modules/bbun/domain/entities/bbun_user_list_entity.dart
@@ -0,0 +1,8 @@
+import 'bbun_user_entity.dart';
+
+class BbunUserListEntity {
+  final int total;
+  final List<BbunUserEntity> list;
+
+  BbunUserListEntity({required this.total, required this.list});
+}
