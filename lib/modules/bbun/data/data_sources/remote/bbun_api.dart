92e92dea152185f00c893ea4d5489e995b60418a
andongyeon <a56634368@gmail.com>
2025-02-17 21:49:33 +0900

    feat: add get bbunline function

---
+++ lib/modules/bbun/data/data_sources/remote/bbun_api.dart
@@ -0,0 +1,21 @@
+import 'package:bbun/modules/bbun/data/models/bbun_user_list_model.dart';
+import 'package:bbun/modules/bbun/data/models/bbun_user_model.dart';
+import 'package:bbun/modules/core/data/dio/bbun_dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'bbun_api.g.dart';
+
+@injectable
+@RestApi(baseUrl: 'user/')
+abstract class BbunApi {
+  @factoryMethod
+  factory BbunApi(BbunDio dio) = _BbunApi;
+
+  @GET('')
+  Future<BbunUserModel> getUser();
+
+  @GET('bbunline')
+  Future<BbunUserListModel> getBbunline();
+}
