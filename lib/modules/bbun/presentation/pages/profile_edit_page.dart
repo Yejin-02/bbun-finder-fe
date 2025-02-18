748b3c26b4e76edf5d0524c2552b8b7223710f14
Yejin-02 <yangyj21@gm.gist.ac.kr>
2025-02-18 11:31:09 +0900

    fix: improve responsiveness of profile edit page

--- lib/modules/bbun/presentation/pages/profile_edit_page.dart
+++ lib/modules/bbun/presentation/pages/profile_edit_page.dart
@@ -62,7 +62,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
               child: Column(
                 children: [
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     height: 257,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
@@ -84,7 +84,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                 child: Column(
                   children: [
                     Container(
-                      width: screenWidth > 430 ? 430 : screenWidth,
+                      width: screenWidth > 475 ? 475 : screenWidth,
                       height: 257,
                       decoration: BoxDecoration(
                           gradient: LinearGradient(
@@ -105,7 +105,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
                   // 뒤로 가기
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     padding: const EdgeInsets.symmetric(horizontal: 36),
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -128,7 +128,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
                   // 내 프로필
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     padding: const EdgeInsets.symmetric(horizontal: 36),
                     child: Text(
                       '내 프로필',
@@ -143,7 +143,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
                   // 개인 정보 동의 안내
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     padding: const EdgeInsets.symmetric(horizontal: 36),
                     child: const Text(
                       "개인 정보 제공에 동의해야 프로필 설정을 완료하고 서비스를 이용할 수 있습니다.",
@@ -168,7 +168,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                   const SizedBox(height: 15),
 
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     padding: const EdgeInsets.symmetric(horizontal: 36),
                     child: GestureDetector(
                       onTap: () {
@@ -201,7 +201,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                   const SizedBox(height: 20),
 
                   Container(
-                    width: screenWidth > 430 ? 430 : screenWidth,
+                    width: screenWidth > 475 ? 475 : screenWidth,
                     padding: const EdgeInsets.symmetric(horizontal: 36),
                     child: Column(
                       children: [
@@ -269,7 +269,6 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
                         // 제출 버튼
                         SizedBox(
-                          width: 317,
                           child: BbunPressable(
                             onPressed: (!isChecked && !dummyIsBbunReg)
                                 ? null
